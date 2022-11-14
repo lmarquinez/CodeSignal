b344e0ffa9189d63f0ecb9d92afa23d6709fd4e6
Lauri <lauri.marquinez@gmail.com>
2022-11-14 15:41:33 +0100

    adjacentElementProduct

---
+++ Arcade/Intro/adjacentElementsProduct.js
@@ -0,0 +1,8 @@
+function adjacentElementsProduct(inputArray) {
+    let max = inputArray[0] * inputArray[1];
+    for (let i = 0; i < inputArray.length - 1; i++) {
+        if (inputArray[i] * inputArray[i + 1] > max)
+            max = inputArray[i] * inputArray[i + 1];
+    }
+    return max
+}
